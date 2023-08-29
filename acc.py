17ee5d088e5d11c3ded787fb6a9e875f8287798a
VoidPlunder <452131330@qq.com>
2023-08-30 02:49:24 +0800

    Create acc.py

---
+++ acc.py
@@ -0,0 +1,14 @@
+import time
+
+minutes = int(input("Enter the number of minutes to focus: "))
+seconds = minutes * 60
+
+while seconds:
+  mins,secs = divmod(seconds,60)
+  timer = '{:02d}'.format(mins,sesc)
+  print(timer,end="\r")
+  time.sleep(1)
+  seconds -= 1
+
+print("Time's up!)
+      
